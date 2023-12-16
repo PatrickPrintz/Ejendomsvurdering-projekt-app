449d2bf2615855014ddfa3c0cfc53faeca534b55
PatrickPrintz <PatrickPrintz@hotmail.dk>
2023-12-16 21:03:34 +0100

    Refactor SHAP-waterfall plot size and data selection

--- App.py
+++ App.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 shap_values = pd.read_csv("shap_values.csv")
 X_test_shh = pd.read_csv("X_test.csv")
 explainer_expected_value = 2678679.5
-
+shap_values.shape
 # Streamlit app
 st.title('SHAP Waterfall Plot App')
 
@@ -17,10 +17,10 @@ nummer = st.number_input('Enter a number:', min_value=0, max_value=len(shap_valu
 # Button to generate and show the plot
 if st.button('Generate Waterfall Plot'):
     # SHAP-waterfall plot
-    plt.figure(figsize=(10, 7))  
-    fig = shap.plots.waterfall(shap.Explanation(values=shap_values.iloc[nummer],
-                                         base_values=explainer_expected_value,
-                                         data=X_test_shh.iloc[nummer]))
+    plt.figure(figsize=(5, 5))  
+    fig = shap.plots.waterfall(shap.Explanation(values=shap_values[nummer],
+                                     base_values=explainer_expected_value,
+                                     data=X_test_shh.iloc[nummer]))
     st.pyplot(fig)
 
 
