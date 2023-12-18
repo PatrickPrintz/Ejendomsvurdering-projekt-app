029d2df085a8eee4f2b05c2c19e37b7fa7dc4f5a
PatrickPrintz <PatrickPrintz@hotmail.dk>
2023-12-18 19:43:12 +0100

    Add energy label mapping and additional property details to the explanation text

--- App.py
+++ App.py
@@ -10,6 +10,8 @@ shap_values = pd.read_csv("shap_values.csv")
 shap_values = shap_values.to_numpy()
 X_test_shh = pd.read_csv("X_test.csv")
 X_test_df = pd.read_csv("X_test_df.csv")
+energylabel_reversed_map = {7: 'A', 6: 'B', 5: 'C', 4: 'D', 3: 'E', 2: 'F', 1: 'G'}
+X_test_df['Energylabel'] = X_test_df['Energylabel'].map(energylabel_reversed_map)
 explainer_expected_value = 2678679.5
 
 # Streamlit app
@@ -27,10 +29,28 @@ if st.button('Generer forklarings plot af vurdering'):
 
     st.write(f"""
     Forklaringen af vurderingen er baseret på en SHAP-waterfall plot.
-            Den indtastede bolig er et {X_test_df.iloc[nummer]['anvendelse']} på {X_test_df.iloc[nummer]['area']} m2, placeret i {X_test_df.iloc[nummer]['Region']}.
+            Den indtastede bolig er et {X_test_df.iloc[nummer]['anvendelse']} på {X_test_df.iloc[nummer]['area']} m2, placeret i {X_test_df.iloc[nummer]['Region']}, med {X_test_df.iloc[nummer]['grund_str']} m2 grund.
+            Boligen består af i alt {X_test_df.iloc['nummer']['badtoi']} badeværelser og toiletter og {X_test_df.iloc[nummer]['rooms']} værelser.
             Boligen er opført i {X_test_df.iloc[nummer]['build_year']} og {ombyg}. Boligens primær varmekilde er {X_test_df.iloc[nummer]['varmesinstallation']} og har en energimærkning på {X_test_df.iloc[nummer]['Energylabel']}.
+            Boligens ydervæg er lavet af {X_test_df.iloc[nummer]['vægmateriale']} og taget består af {X_test_df.iloc[nummer]['tagkode']}.
             
-            HELLO!!!!
+
+            Boligen er placeret {X_test_df.iloc['nummer']['dist_coast']} km. fra kysten, {X_test_df.iloc[nummer]['dist_highway']} km. fra nærmeste motorvej, 
+            {X_test_df.iloc[nummer]['dist_railroads']} km. fra nærmeste jernbane, {X_test_df.iloc[nummer]['dist_airports']} km. fra nærmeste lufthavn.
+
+            Nærmeste skole er {X_test_df.iloc[nummer]['dist_school']} km. væk, nærmeste universitet er {X_test_df.iloc[nummer]['dist_university']} km. væk og nærmeste insitution eller børnehave er {X_test_df.iloc[nummer]['dist_kindergarten']} km. væk.
+
+            Boligen er ligeledes placeret tæt på naturen, hvor der er {X_test_df.iloc[nummer]['dist_forests']} km. til nærmeste skov og {X_test_df.iloc[nummer]['waterlines']} km. til nærmeste sø eller å.
+
+            Naboområdets pris ligger på omstrent {X_test_df.iloc[nummer]['weighted_price'].round(0)} kr. vægtet efter afstanden til boligen.
+
+            Boliger er placeret ved {X_test_df.iloc[nummer]['longitude']} længdegrad og {X_test_df.iloc[nummer]['latitude']} breddegrad.
+
+            
+            Nedenfor fremgår en forklaring på modellets vurdering af boligen.
+
+
+
             """)
     # SHAP-waterfall plot
     plt.figure(figsize=(5, 5))  
